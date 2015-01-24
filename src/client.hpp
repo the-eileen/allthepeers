07b094b0bdcea51c502952e9943baedd7e0a46a7
joshstclair <josh.jds@gmail.com>
2015-01-23 16:39:42 -0800

    THE BEGINNING!!

---
+++ src/client.hpp
@@ -0,0 +1,39 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2014,  Regents of the University of California
+ *
+ * This file is part of Simple BT.
+ * See AUTHORS.md for complete list of Simple BT authors and contributors.
+ *
+ * NSL is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * NSL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * NSL, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * \author Yingdi Yu <yingdi@cs.ucla.edu>
+ */
+
+#ifndef SBT_CLIENT_HPP
+#define SBT_CLIENT_HPP
+
+#include "common.hpp"
+
+namespace sbt {
+
+class Client
+{
+public:
+  Client(const std::string& port, const std::string& torrent)
+  {
+  }
+};
+
+} // namespace sbt
+
+#endif // SBT_CLIENT_HPP
