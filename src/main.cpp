c802761101030cf854939f0decc939d3a8ac899c
Eileen <eileenthe@gmail.com>
2015-02-13 23:29:30 -0800

    handshake in separate function

--- src/main.cpp
+++ src/main.cpp
@@ -49,6 +49,35 @@
   using namespace sbt;
   using namespace msg;
 
+int shakeHands(Peer pr, Client client){ //takes peer and client, returns socket created for this peer
+  MetaInfo* metainfo = client.m_info;
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0); //create socket
+  
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(pr.m_port);     
+  const char* peerip = pr.m_ip.c_str();
+  addr.sin_addr.s_addr = inet_addr(peerip);
+  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+  if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+    perror("connect");
+    //return 2;
+    }
+
+  char rshake[68] = {0}; //find a way to save this somewhere
+  HandShake hshake = HandShake(metainfo->getHash(), client.m_strId);
+  char* buf = (char*)hshake.encode()->buf();
+  if(send(sockfd, buf, 68, 0) == -1)
+    perror("send");
+  if (recv(sockfd, rshake, sizeof(rshake), 0) == -1) 
+    perror("recv");
+
+  cerr << "rshake is" << rshake;
+  fprintf(stderr, "PRINT");
+  ConstBufferPtr peerShake = std::make_shared<Buffer>(rshake, 68);
+  return sockfd;
+}
+
 void makeGetRequest(Client client){
   MetaInfo* metainfo = client.m_info;
   HttpRequest req;
@@ -193,33 +222,13 @@ void makeGetRequest(Client client){
         peerList.push_back(Peer(*it, numOfPieces));
     }
     
-      char rshake[68] = {0};
+    
     
     //Eileen start
    vector<int> socketList;
 
 for(std::vector<Peer>::iterator it = peerList.begin(); it != peerList.end() ; it++){
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  socketList.push_back(sockfd);
-  struct sockaddr_in addr;
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(it->m_port);     // short, network byte order
-  const char* peerip = it->m_ip.c_str();
-  addr.sin_addr.s_addr = inet_addr(peerip);
-  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
-  if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
-    perror("connect");
-    //return 2;
-    }
-
-
-  HandShake hshake = HandShake(metainfo->getHash(), client.m_strId);
-  char* buf = (char*)hshake.encode()->buf();
-  if(send(sockfd, buf, 68, 0) == -1)
-    perror("send");
-  if (recv(sockfd, rshake, sizeof(rshake), 0) == -1) 
-    perror("recv");
-  ConstBufferPtr peerShake = std::make_shared<Buffer>(rshake, 68);
+   socketList.push_back(shakeHands(*it,client));
 }
 //Eileen end
 
